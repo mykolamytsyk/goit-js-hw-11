d86752db1683ef78598c01114da28055a4239cfe
mykolamytsyk <138970828+mykolamytsyk@users.noreply.github.com>
2023-11-22 13:36:04 +0200

    ddf

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
-// Fetch images from Pixabay API using Axios
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '34532945-6dd9e50d65c600f2d5972702b';
 
-async function fetchImages(name, page, perPage) {
-  const baseURL = 'https://pixabay.com/api/';
-  const key = '28143013-44919de38ad9e5402793063fb';
-
+export async function fetchImages(value, page = 1, perPage = 40) {
   try {
     const response = await axios.get(
-      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+      `${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
     );
-    return response.data;
+    return response;
   } catch (error) {
-    console.log('ERROR: ' + error);
+    console.log(error);
   }
 }
-
-export { fetchImages };
